02de26b3283ee9575c48516b834ad34b3f1906cf
Lucas Andrade <llucasandradd@gmail.com>
2021-10-28 01:02:06 -0300

    Finish initial config to have apollo on the client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+} from '@apollo/client';
+import { cache } from 'src/utils/cache';
+import { typeDefs } from 'src/utils/typeDefs';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql',
+  typeDefs,
+});
+
 export const App = () => {
   return (
-    <div>Pokedex App</div>
+    <ApolloProvider client={client}>
+      <div>Pokedex App</div>
+    </ApolloProvider>
   );
-}
+};
