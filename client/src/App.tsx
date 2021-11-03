e047310209eb75a707c915837060122af160f064
Lucas Andrade <llucasandradd@gmail.com>
2021-11-03 01:34:44 -0300

    Start to add the request into my project to load pokemons on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {
   ApolloClient,
   NormalizedCacheObject,
   ApolloProvider,
+  gql,
 } from '@apollo/client';
 import { cache } from 'src/utils/cache';
 import { typeDefs } from 'src/utils/typeDefs';
@@ -11,6 +12,19 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   typeDefs,
 });
+
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        pokemons(page: 1, items: 6) {
+          name
+          img
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 export const App = () => {
   return (
