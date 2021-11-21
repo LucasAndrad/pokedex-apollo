593c56b2dd48cfbe420f1a6e26a8816b70962601
Lucas Andrade <llucasandradd@gmail.com>
2021-11-21 16:07:18 -0300

    Start to create the pokemon type info

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,6 +7,30 @@ const typeDefs = gql`
     img: String
   }
 
+  type AbilityShort {
+    name: String
+    url: String
+  }
+
+  type TypeShort {
+    name: String
+    url: String
+  }
+
+  type Pokemon {
+    id: Int
+    abilities: [AbilityShort]
+    name: String
+    height: Int
+    width: Int
+    types: [TypeShort]
+    imgs: {
+      default: String
+      game: String
+      gameShiny: String
+    }
+  }
+
   type Query {
     pokemons(items: Int, page: Int): [PokemonShort]
   }
