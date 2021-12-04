fd3e2458184780c2cde77d7e1b34676970e1592b
Lucas Andrade <llucasandradd@gmail.com>
2021-12-04 17:16:26 -0300

    Start to add login mutation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 import { CreateAccount } from 'src/components';
+
+const LOGIN_MUTATION = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      error
+    }
+  }
+`;
 
 export const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
+  const [loginUser, { data, loading, error }] = useMutation(LOGIN_MUTATION);
+
+  const onLogin = () => {
+    loginUser({ variables: { email, password } });
+  };
 
   return (
     <div>
@@ -20,7 +36,9 @@ export const Login = () => {
         onChange={(event: any) => setPassword(event.target.value)}
       />
 
-      <button>Login</button>
+      <button onClick={onLogin}>Login</button>
+      {loading && <p>Loading</p>}
+      {/* {data && <p>Success: {data}</p>} */}
 
       <div style={{
         width: '100%', height: '1px', background: 'black', margin: '20px 0',
