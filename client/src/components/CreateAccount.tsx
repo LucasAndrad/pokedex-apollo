429bba022aca68aff2f160145b6c3a924e51e8e3
Lucas Andrade <llucasandradd@gmail.com>
2021-12-04 17:07:23 -0300

    Add create account form structure

--- client/src/components/CreateAccount.tsx
+++ client/src/components/CreateAccount.tsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
-import { CreateAccount } from 'src/components';
 
-export const Login = () => {
+export const CreateAccount = () => {
   const [email, setEmail] = useState();
+  const [name, setName] = useState();
   const [password, setPassword] = useState();
 
   return (
     <div>
-      <h6>Login</h6>
+      <h6>Create Account</h6>
       <input
-        type="email"
+        type="text"
+        placeholder="Email"
         value={email}
         onChange={(event: any) => setEmail(event.target.value)}
       />
 
       <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(event: any) => setName(event.target.value)}
+      />
+
+      <input
         type="password"
+        placeholder="Password"
         value={password}
         onChange={(event: any) => setPassword(event.target.value)}
       />
 
-      <button>Login</button>
-
-      <div style={{
-        width: '100%', height: '1px', background: 'black', margin: '20px 0',
-      }} />
-
-      <CreateAccount />
+      <button>Create Account</button>
     </div>
   );
 };
