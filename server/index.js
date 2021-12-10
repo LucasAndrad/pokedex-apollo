7aef90103dcb8119a24e715aeee302a873a2f2ec
Lucas Andrade <llucasandradd@gmail.com>
2021-12-10 09:19:32 -0300

    Fix login request check, not validating user token for login action

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ const { parseJwt } = require('./src/utils/auth');
       usersAPI: new UserAPI({ store })
     }),
     context: ({ req }) => {
+      if (req?.body?.operationName === 'Login') return;
+
       const authorizationHeader = req.headers.authorization || '';
       const token = authorizationHeader.split('Bearer ')[1];
       const user = parseJwt(token);
